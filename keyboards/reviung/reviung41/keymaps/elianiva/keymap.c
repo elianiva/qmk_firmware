09b1d8feb5670ac8adeb9677aa0aa3cb75233553
elianiva <dicha.arkana03@gmail.com>
2024-03-11 22:35:12 +0700

    refactor: adjust media keys

--- keyboards/reviung/reviung41/keymaps/elianiva/keymap.c
+++ keyboards/reviung/reviung41/keymaps/elianiva/keymap.c
@@ -72,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+--------+--------+--------+--------|--------+-                    |--------| -------+--------+--------+--------+--------+-
      KC_F11,   KC_F4,   KC_F5,   KC_F6, KC_ACL1, XXXXXXX,                       XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_MNXT,
   //|------+--------+--------+--------+--------|--------+-                    |--------| -------+--------+--------+--------+--------+-
-     KC_F10,   KC_F1,   KC_F2,   KC_F3, KC_ACL2, XXXXXXX,                       XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,
+     KC_F10,   KC_F1,   KC_F2,   KC_F3, KC_ACL2, XXXXXXX,                       KC_BTN3, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                               XXXXXXX, KC_BTN3, XXXXXXX, KC_BTN1, KC_BTN2
+                                               XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2
                                       //`--------------------------'  `--------------------------'
   ),
 
